5d0764336f6a21a508d218fc07f7a32046d99118
lifangyin <mail17650565536@163.com>
2022-10-01 22:21:18 +0800

    重新理解图解

--- LeeCode/01字符串轮转/index1.js
+++ LeeCode/01字符串轮转/index1.js
@@ -1,10 +1,20 @@
+
 var isFlipedString = function(s1, s2) {
-    var midObjArr = []
+    if(s1.length>100000||s2.length>100000){return false}
     if(s1.length!==s2.length){return false}
-    // console.log(s1.length);
+    if(s1==s2&&s1 === ''){return true}
+    if(s1==s2&&s1.length==1){return true}
+    console.log(s1.length);
+    if(s1.length ==  2&& s2.length==2){
+        if(s1[0]==s2[1]&&s1[1]==s2[0]){
+            return true
+        }else{
+            return false
+        }
+    }
+    var midObjArr = []
     // 中心点的字符串 可能的位数 1 > s1.length-1
     for(var midLen = 1 ;midLen<s1.length-1;midLen++){
-        
         for(var i = 0;i<=s1.length-midLen;i++){
             // 第一个从0开始到 i位
             // 中心串 从i开始 截取midlen+i位
@@ -12,20 +22,20 @@ var isFlipedString = function(s1, s2) {
             let str1 = s1.slice(0,i)
             let midStr =  s1.slice(i,midLen+i)
             let str3 = s1.slice(midLen + i,s1.length)
-            // console.log(i,midLen);
+            console.log(i,midLen);
             // console.log(str1,'--',midStr,'--',str3);
             let finalStr = str3 + midStr + str1
             midObjArr.push( finalStr )
         }
     } 
-    // console.log(midObjArr);
+    console.log(midObjArr);
     
     return midObjArr.includes(s2)
 
 
 };
 
-var s1 = "waterbottle", s2 = "erbottlewat"
+var s1 = "abcdefghijk", s2 = "jkdefghiabc"
 
 
 console.log(isFlipedString(s1,s2));
---
+++ LeeCode/01字符串轮转/index2.js
@@ -0,0 +1,12 @@
+var s1 = "waterbottle", s2 = "erbottlewat"
+var isFlipedString = function(s1, s2) {
+    let str = s2 +s2
+    let i = str.indexOf(s1)
+    let str1 = str.slice(0,i)
+    let str2 = str.slice(i+s1.length,str.length)
+    return i>-1 && str2 + str1 == s1
+   
+
+};
+var res = isFlipedString(s1,s2)
+console.log();
